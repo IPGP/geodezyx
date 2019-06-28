2248a7c6eeb4cb4330858f65d2ab76f3470a040d
PSTPX1 <psakicki@TPX1-GFZ>
2019-06-28 14:40:20 +0200

    add structure and convert files

---
+++ utils/shell_like.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 14:28:06 2019
+
+@author: psakicki
+"""
+
---
+++ utils/utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 14:27:56 2019
+
+@author: psakicki
+"""
+
