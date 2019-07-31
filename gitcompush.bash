6a2ebdf2068b5eec8b7d2249ee99ea95d591696f
PSTPX1 <psakicki@TPX1-GFZ>
2019-07-31 13:20:35 +0200

    psakicki TPX1-GFZ 2019-07-31 13:20:35

--- gitcompush.bash
+++ gitcompush.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 hosnam=`hostname`
+user="$USER"
 dat=`date '+%Y-%m-%d %H:%M:%S'`
 rm -fv lib/*pyc
-git commit -a -m "$hosnam $dat"
+git commit -a -m "$user $hosnam $dat"
 git push origin master
