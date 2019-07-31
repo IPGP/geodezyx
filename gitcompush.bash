13f4e7309fec43f8a747c2a3c25235c2a6ca8669
PSTPX1 <psakicki@TPX1-GFZ>
2019-07-31 13:30:52 +0200

    psakicki TPX1-GFZ 2019-07-31 13:30:52

--- gitcompush.bash
+++ gitcompush.bash
@@ -3,6 +3,7 @@
 hosnam=`hostname`
 user="$USER"
 dat=`date '+%Y-%m-%d %H:%M:%S'`
-rm -fv lib/*pyc
+#rm -fv lib/*pyc
+find . -name "__pycache__" -exec rm -vr {} +
 git commit -a -m "$user $hosnam $dat"
 git push origin master
