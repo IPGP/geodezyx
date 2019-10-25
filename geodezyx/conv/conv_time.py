bd7aad8bfdb1e45cbf21fe86cabf12382fdcd261
PSTPX1 <psakicki@TPX1-GFZ>
2019-10-25 21:53:25 +0200

    psakicki TPX1-GFZ 2019-10-25 21:53:25

--- geodezyx/conv/conv_time.py
+++ geodezyx/conv/conv_time.py
@@ -781,7 +781,7 @@ def dt2gpstime(dtin,dayinweek=True,inp_ref="utc"):
         Is a list of tuple if the input is an iterable
     """
     
-    if cnv_gen.is_iterable(dtin):
+    if utils.is_iterable(dtin):
         return [dt2gpstime(e) for e in dtin]
         
     else:
