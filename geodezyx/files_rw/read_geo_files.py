d3341d244b3612128d6521927b4b15db6463975d
chaiyap@gfz-potsdam.de <chaiyap@kg84.gfz-potsdam.de>
2020-03-26 18:06:12 +0100

    Added fucntion for reading RINEX Met files

--- geodezyx/files_rw/read_geo_files.py
+++ geodezyx/files_rw/read_geo_files.py
@@ -30,7 +30,7 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 from geodezyx import *                   # Import the GeodeZYX modules
 from geodezyx.externlib import *         # Import the external modules
 from geodezyx.megalib.megalib import *   # Import the legacy modules names
-
+from geodezyx import utils,re
 import linecache
 
 #import geodezyx.megalib.geodetik as geok
@@ -1814,6 +1814,55 @@ def read_bernese_trp(trpfile):
     DF[cols_num] = DF[cols_num].apply(pd.to_numeric, errors='coerce')
     DF.drop(['year','month','day','hour','minute','second'], axis=1,inplace=True)
     return DF
+    
+
+def read_rinex_met(metfile):
+    """
+    This function reads RINEX Meteorological files and convert to Pandas DataFrame
+    
+    Parameter
+    ----------
+    metfile:
+        Path of RINEX Meteorological file in List/String (e.g. made with glob)
+    
+    Return
+    ----------
+    DF:
+        Meteorological data in DataFrame
+    
+    Notes
+    ----------
+        Written by Chaiyaporn Kitpracha
+    """
+    if utils.is_iterable(metfile):
+        merge_df = pd.DataFrame()
+        for metfile_m in zip(metfile):
+            met_df = read_rinex_met_2(metfile_m)
+            merge_df = pd.concat([merge_df,met_df])
+        return merge_df
+    else:
+        met_df = read_rinex_met_2(metfile)
+        return met_df
+
+def read_rinex_met_2(metfile):
+    ln=0
+    for line in open(metfile,"r",encoding = "ISO-8859-1"):
+        if re.compile('MARKER NAME').search(line):
+            marker = line.split()[0]
+        if re.compile('# / TYPES OF OBSERV').search(line):
+            tmp = line.split()
+            headers = tmp[1:int(tmp[0])+1]
+        if re.compile('END OF HEADER').search(line):
+            break
+        ln = ln+1
+        
+    df = pd.read_fwf(metfile,skiprows=range(0,ln+1),delim_whitespace=True,skipinitialspace=True,names=['year','month','day','hour','minute','second']+headers)
+    df['year'] = df['year'] + 2000 if df['year'].any() <= 79 else df['year'].any() + 1900
+
+    df['epoch'] = pd.to_datetime(df[['year','month','day','hour','minute','second']],errors='coerce')
+    df.drop(['year','month','day','hour','minute','second'], axis=1,inplace=True)
+    df.set_index('epoch',inplace=True)
+    return df
 
 
 
