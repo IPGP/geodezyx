7cb04c39a16a7fe58b539e9944b141ec00f3610e
PSTPX1 <psakicki@TPX1-GFZ>
2021-05-20 11:40:32 +0200

    psakicki TPX1-GFZ 2021-05-20 11:40:32

--- gitcompush.bash
+++ gitcompush.bash
@@ -5,6 +5,6 @@ user="$USER"
 dat=`date '+%Y-%m-%d %H:%M:%S'`
 #rm -fv lib/*pyc
 find . -name "*.pyc" -exec rm -fv "{}" \;
-find . -name "__pycache__" -exec rm -vr {} +
+find . -name "__pycache__" -exec rm -fvr "{}" \;
 git commit -a -m "$user $hosnam $dat"
 git push origin master
