6a47118b79aabde06a0334bd51ddf8028cfc82a9
PSTPX1 <psakicki@TPX1-GFZ>
2021-09-07 14:27:07 +0200

    psakic TPX1-GFZ 2021-09-07 14:27:07

--- geodezyx/conv/__init__.py
+++ geodezyx/conv/__init__.py
@@ -1,6 +1,7 @@
 from .conv_coords            import *
 from .conv_angle             import *
 from .conv_geometric         import *
+from .conv_interpolators     import *
 from .conv_proj_carto        import *
 from .conv_rotation_matrices import *
 from .conv_time              import *  
