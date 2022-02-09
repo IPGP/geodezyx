3fdb78ee3fe152f646156453419d8a19c6585be0
PSTPX1 <psakicki@TPX1-GFZ>
2022-02-09 15:56:18 +0100

    psakic TPX1-GFZ 2022-02-09 15:56:18

--- setup.py
+++ setup.py
@@ -187,10 +187,12 @@ PyPi project: https://pypi.org/project/geodezyx
     #
     # Similar to `install_requires` above, these must be valid existing
     # projects.
-    #extras_require={  # Optional
+    extras_require={  # Optional
     #    'dev': ['check-manifest'],
     #    'test': ['coverage'],
-    #},
+         'full': ['netCDF4'],
+
+    },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.
