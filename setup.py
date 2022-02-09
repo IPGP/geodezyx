570d4ab85bd27dd2c597d5b18c03420247c2cb44
PSTPX1 <psakicki@TPX1-GFZ>
2022-02-09 15:59:23 +0100

    psakic TPX1-GFZ 2022-02-09 15:59:23

--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ PyPi project: https://pypi.org/project/geodezyx
                       'tabulate',
                       'vincenty',
                       'wheel',
-                      ]  # Optional
+                      ],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
