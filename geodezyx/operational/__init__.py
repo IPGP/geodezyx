6f67bb194fd854512e1a27cc9d5b53b94df5cc22
Gustavo Mansur <mansur@gfz-potsdam.de>
2022-10-11 10:06:00 +0200

    mansur kg163 2022-10-11 10:06:00

--- geodezyx/operational/__init__.py
+++ geodezyx/operational/__init__.py
@@ -2,11 +2,11 @@ from .anubis_frontend       import *
 from .cluster_gfz           import *
 from .gins_runner           import *
 # from .gnss_downloader     import *
-# from download_cddis         import *
-# from download_find_files    import *
-from download_prods         import *
-from download_rinex         import *
-from download_utils         import *
+from .download_cddis         import *
+from .download_find_files    import *
+from .download_prods         import *
+from .download_rinex         import *
+from .download_utils         import *
 from .hector_frontend       import *
 from .midas_frontend        import *
 from .rinex_lister_plotter  import *
