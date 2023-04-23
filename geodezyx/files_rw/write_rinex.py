68006bdfdcecb26f04c6c8e02559f86b48b8287c
PSTPX1 <psakicki@TPX1-GFZ>
2023-04-23 22:59:26 +0200

    write rinex

--- geodezyx/files_rw/write_rinex.py
+++ geodezyx/files_rw/write_rinex.py
@@ -21,15 +21,47 @@ https://github.com/GeodeZYX/GeodeZYX-Toolbox_v4
 #### External modules
 import numpy as np
 import pandas as pd
+from io import StringIO
+
+import rinexfile
 
 #### geodeZYX modules
-from geodezyx import files_rw
+from geodezyx import files_rw, utils
 
 
 #### Import the logger
 import logging
 log = logging.getLogger(__name__)
 
+
+def write_rinex3(DFrnx_in,rnx_header=""):
+    
+    if utils.is_iterable(DFrnx_in) and not type(DFrnx_in) is pd.DataFrame:
+        DFrnx = pd.concat(DFrnx_in)
+    else:
+        DFrnx = DFrnx_in
+        
+    rnx_body, dict_sys_obs = write_rinex3_body(DFrnx)
+    dict_sys_obs = files_rw.dict_sys_obs_clean_LLI_SSI(dict_sys_obs)
+    
+    if not rnx_header:
+        rnx_head = header_rinex3_generic()
+    else:
+        rnx_head = rnx_header
+        
+    rnx_concat = rnx_head + rnx_body 
+    rnx_concat = StringIO(rnx_concat)
+    
+    RNX = rinexfile.RinexFile(rnx_concat)
+    
+    ###### low level changes
+    ## Observables
+    RNX.mod_sys_obs_types(dict_sys_obs)
+    ## Time first/last obs
+    RNX.mod_time_obs(RNX.start_date,)
+    
+    return RNX#.get_rinex_as_string()
+    
 
 def write_rinex3_body(DFrnx_in):
     """
@@ -44,9 +76,16 @@ def write_rinex3_body(DFrnx_in):
     -------
     rnx_str_out : str
         The RINEX data records as string.
+        
+    dict_sys_obs : dict
+        A dictionnary with GNSS system as key (G,R,E...).
+        And the observables for each system as values
+        (gives the order of the observables)
+        
     """
     
-    obsdic = files_rw.observables_dict_per_sys(DFrnx_in)
+    dict_sys_obs = files_rw.observables_dict_per_sys(DFrnx_in)
+    DFrnx_in = _lli_ssi_nan_to_0(DFrnx_in)
     
     DFepoc = DFrnx_in.groupby("epoch")
     
@@ -57,20 +96,22 @@ def write_rinex3_body(DFrnx_in):
         DFsys = grpepoc.groupby('sys')
         
         for sys, grpsys in DFsys:        
-            grpsys2 = grpsys[["prn"] + obsdic[sys]].copy()
-            l = "{:3s}" + "{:14.3f}{:1d}{:1d}" * int(len(obsdic[sys])/3)
+            grpsys2 = grpsys[["prn"] + dict_sys_obs[sys]].copy()
+            l = "{:3s}" + "{:14.3f}{:1d}{:1d}" * int(len(dict_sys_obs[sys])/3)
             grpsys2 = grpsys2.apply(lambda x:l.format(*x.values),axis=1)
             grpsys2 = grpsys2.apply(_lli_ssi_0_to_blank)
             
         lines_stk = lines_stk + list(grpsys2)
         
-    rnx_str_out = "\n".append(lines_stk)
+    rnx_str_out = "\n".join(lines_stk)
     
-    return rnx_str_out
+    return rnx_str_out, dict_sys_obs
     
 
 
-def write_epoch_rinex3(epoch,nsats,epoch_flag=0,clk_offset=0.):
+def write_epoch_rinex3(epoch,nsats,
+                       epoch_flag=0,
+                       clk_offset=0.):
     """
     Write the epoch line in the RINEX3 format
 
@@ -96,7 +137,7 @@ def write_epoch_rinex3(epoch,nsats,epoch_flag=0,clk_offset=0.):
         output string line.
     """
     
-    l = "> {:4d} {:2d} {:2d} {:2d} {:2d} {:11.7f}  {:1d}{:3d}      {:15.12f}"
+    l = "> {:4d} {:2d} {:2d} {:2d} {:2d}{:11.7f}  {:1d}{:3d}      {:15.12f}"
     
     lout = l.format(epoch.year,
                     epoch.month,
@@ -146,3 +187,21 @@ def _lli_ssi_0_to_blank(rnxlinestr_in):
             rnxlinelist[i] = " "
             
     return("".join(rnxlinelist))
+
+
+def header_rinex3_generic():
+    head =  """     3.05           OBSERVATION DATA    M (MIXED)           RINEX VERSION / TYPE
+GR50 V4.31          XXXXXX              20200210 235942 UTC PGM / RUN BY / DATE
+XXXX                                                        MARKER NAME
+        0.0000        0.0000        0.0000                  APPROX POSITION XYZ
+G    9 XXX XXX XXX XXX XXX XXX XXX XXX XXX                  SYS / # / OBS TYPES 
+00000M000                                                   MARKER NUMBER
+        0.0000        0.0000        0.0000                  ANTENNA: DELTA H/E/N
+Automatic           XXXX                                    OBSERVER / AGENCY
+XXXXXXX                                                     ANT # / TYPE
+     0                                                      RCV CLOCK OFFS APPL
+XXXXXXX                                                     REC # / TYPE / VERS
+  1980     1    01     0     0    0.0000000     GPS         TIME OF FIRST OBS
+                                                            END OF HEADER
+"""
+    return head
