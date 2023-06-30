e4a0c86d8666f1303d6bb5cddaeba4dd7e5f8fb0
PSTPX1 <psakicki@TPX1-GFZ>
2023-06-30 15:26:03 +0200

    psakic TPX1-GFZ 2023-06-30 15:26:02

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4.3.3',  # Required
+    version='0.4.3.5',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -94,7 +94,7 @@ PyPi project: https://pypi.org/project/geodezyx
 
     # This should be a valid email address corresponding to the author listed
     # above.
-    author_email='pierre.sakic@gfz-potsdam.de',  # Optional
+    author_email='sakic@ipgp.fr',  # Optional
 
     # Classifiers help users find your project by categorizing it.
     #
@@ -163,6 +163,7 @@ PyPi project: https://pypi.org/project/geodezyx
                       'collection',
                       'colorlog',
                       #'ftplib',
+                      'hatanaka',
                       #'kepler.py',
                       'natsort',
                       'matplotlib',
