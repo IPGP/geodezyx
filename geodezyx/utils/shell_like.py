072edc7234e2505840073949ed84a020929c4ab7
Pierre Sakic <sakic@ipgp.fr>
2024-04-05 07:20:52 +0000

    stuffs

--- geodezyx/utils/shell_like.py
+++ geodezyx/utils/shell_like.py
@@ -93,10 +93,11 @@ def tail(filename, count=1, offset=1024):
     Depending on the length of your lines, you will want to modify offset
     to get better performance.
     """
+    
     f_size = os.stat(filename).st_size
     if f_size == 0:
         return []
-    with open(filename, 'r') as f:
+    with open(filename, 'r',errors='ignore') as f:
         if f_size <= offset:
             offset = int(f_size / 2)
         while True:
@@ -118,7 +119,13 @@ def head(filename, count=1):
     This one is fairly trivial to implement but it is here for completeness.
     """
     with open(filename, 'r') as f:
-        lines = [f.readline() for line in range(1, count+1)]
+        lines = []
+        for iline in range(1, count+1):
+            try:
+                lines.append(f.readline())
+            except Exception as e:
+                log.warn(e)
+                continue
         return list(filter(len, lines))
 
 
