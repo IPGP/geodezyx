d904fe1f5bdede5410873a060cb9569542b0f5b0
PSTPX1 <psakicki@TPX1-GFZ>
2024-10-17 21:31:08 +0200

    better pride prod

--- geodezyx/operational/pride_pppar_frontend.py
+++ geodezyx/operational/pride_pppar_frontend.py
@@ -559,11 +559,12 @@ def pride_pppar_runner(rnx_path_list,
     date_list = [conv.rinexname2dt(rnx) - dt.timedelta(seconds=0) for rnx in rnx_path_list] 
 
     ## when we do multi_process, we download the products first, all at once, to avoid conflicts
-    _ = dl_prods_pride_pppar(prod_parent_dir,date_list,prod_ac_name)
-    _ = dl_brdc_pride_pppar(prod_parent_dir, date_list)
-    if dl_prods_only:
-        log.info("products downloaded, exiting (dl_prods_only is activated.)")
-        return None
+    if dl_prods:
+        _ = dl_prods_pride_pppar(prod_parent_dir,date_list,prod_ac_name)
+        _ = dl_brdc_pride_pppar(prod_parent_dir, date_list)
+        if dl_prods_only:
+            log.info("products downloaded, exiting (dl_prods_only is activated.)")
+            return None
 
     kwargs_list = []
     for rnx_path in rnx_path_list:
