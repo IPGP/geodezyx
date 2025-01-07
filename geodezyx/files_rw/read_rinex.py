40b8e1787f222614d8d9feff6510a24120031ea2
PSTPX1 <psakicki@TPX1-GFZ>
2025-01-07 17:57:25 +0100

    comment open rnx fct for v2 and 3 + attempt to faster rnx3

--- geodezyx/files_rw/read_rinex.py
+++ geodezyx/files_rw/read_rinex.py
@@ -60,28 +60,11 @@ def read_rinex2_obs(rnx_in, set_index=None):
     df_rnx_obs : Pandas DataFrame / GeodeZYX's RINEX format
     """
 
-    #### open block
-    try:
-        rnx_wrk = hatanaka.decompress(rnx_in)
-    except:
-        rnx_wrk = rnx_in
-        pass
+    #### open rinex
+    lines, epochs, filename = open_rinex(rnx_in)
 
-    lines = utils.open_readlines_smart(rnx_wrk, verbose=False)
-    epochs = operational.rinex_read_epoch(rnx_wrk, out_index=True)
-    if type(rnx_in) is str or type(rnx_in) is pathlib.Path:
-        filename = os.path.basename(rnx_in)
-    else:
-        filename = "unknown filename"
-
-    #### Split header and Observation body
-    i_end_header = 0
-    for il, l in enumerate(lines):
-        if "END OF HEADER" in l:
-            i_end_header = il
-            break
-
-    lines_header = lines[: i_end_header + 1]
+    ### get the header
+    lines_header = _header_reader(lines)
 
     # get the rinex type (mono or multi GNSS)
     rnx_typ = lines_header[0][40]
@@ -129,8 +112,11 @@ def read_rinex2_obs(rnx_in, set_index=None):
         epoc, nsat, lineconcat, sats_split, iline_sats_end = _sats_find(lines_epoc)
 
         ### for each sat, merge the breaked lines
-        lines_obs = [e.ljust(80).replace("\r", "").replace("\n", "") for e in lines_epoc[iline_sats_end + 1: iline_end]]
+        lines_obs = [re.sub(r"[\r\n]+", "", e.ljust(80)) for e in lines_epoc[iline_sats_end + 1: iline_end]]
         lines_obs_merg = ["".join(lines_obs[n * nlines_for_obs:(n + 1) * nlines_for_obs]) for n in range(nsat)]
+
+        # .replace("\r", "").replace("\n", "") i.e. re.sub(r"[\r\n]+", "", s) IS NOT .strip() !!
+        # .strip() removes the spaces at the beginning and end of the string, and they should be kept!
 
         ## read the epoch block using pandas' fixed width reader
         b = StringIO("\n".join(lines_obs_merg))
@@ -189,29 +175,11 @@ def read_rinex3_obs(rnx_in, set_index=None):
     df_rnxobs : Pandas DataFrame / GeodeZYX's RINEX format
     """
 
-    #### open block
-    try:
-        rnx_wrk = hatanaka.decompress(rnx_in)
-    except:
-        rnx_wrk = rnx_in
-        pass
+    #### open rinex
+    lines, epochs, filename = open_rinex(rnx_in)
 
-    lines = utils.open_readlines_smart(rnx_wrk)
-    epochs = operational.rinex_read_epoch(rnx_wrk, out_index=True)
-    if type(rnx_in) is str or type(rnx_in) is pathlib.Path:
-        filename = os.path.basename(rnx_in)
-    else:
-        filename = "unknown filename"
-
-    #### Split header and Observation body
-    i_end_header = 0
-    for il, l in enumerate(lines):
-        if "END OF HEADER" in l:
-            i_end_header = il
-            break
-
-    lines_header = lines[: i_end_header + 1]
-    # LINES_obs = lines[i_end_header:]
+    ### get the header
+    lines_header = _header_reader(lines)
 
     #### get the systems and observations
     lines_sys = [l for l in lines_header if "SYS / # / OBS TYPES" in l]
@@ -262,17 +230,16 @@ def read_rinex3_obs(rnx_in, set_index=None):
         else:
             iline_end = epochs[iepoc + 1, 1]
 
-        lines_epoc = lines[iline_start:iline_end]
-        ###  Remove CR (Carriage Return) and LF (Line Feed)
-        # lines_epoc = [l.replace('\r', '') for l in lines_epoc]
-        # lines_epoc = [l.replace('\n', '') for l in lines_epoc]
-        lines_epoc = [l.strip() for l in lines_epoc]
+        lines_epoc = [re.sub(r"[\r\n]+", "", e) for e in lines[iline_start:iline_end]]
+
+        # .replace("\r", "").replace("\n", "") i.e. re.sub(r"[\r\n]+", "", s) IS NOT .strip() !!
+        # .strip() removes the spaces at the beginning and end of the string, and they should be kept!
 
         ## read the epoch block using pandas' fixed width reader
-        B = StringIO("\n".join(lines_epoc))
+        b = StringIO("\n".join(lines_epoc))
 
         columns_width = [3] + nobs_max * [14, 1, 1]
-        df_epoch = pd.read_fwf(B, header=None, widths=columns_width)
+        df_epoch = pd.read_fwf(b, header=None, widths=columns_width)
 
         # one add the system to use groupby
         #df_epoch_sysadded = df_epoch.copy()
@@ -311,8 +278,171 @@ def read_rinex3_obs(rnx_in, set_index=None):
 
     return df_rnx_obs
 
+def read_rinex3_obs_dev(rnx_in, set_index=None):
+    """
+    Read a RINEX Observation, version 3 or 4
+
+    Parameters
+    ----------
+    rnx_in : see below
+        input RINEX.
+        can be the path of a RINEX file as string or as Path object,
+        or directly the RINEX content as a string, bytes, StringIO object or a
+        list of lines
+    set_index : str or list of str, optional
+        define the columns for the index.
+        If None, the output DataFrame is "flat", with integer index
+        ["epoch","prn"] for instance set the epoch and the prn as MultiIndex
+        The default is None.
+
+    Returns
+    -------
+    df_rnxobs : Pandas DataFrame / GeodeZYX's RINEX format
+    """
+
+    #### open rinex
+    lines, epochs, filename = open_rinex(rnx_in)
+
+    ### get the header
+    lines_header = _header_reader(lines)
+
+    #### get the systems and observations
+    lines_sys = [l for l in lines_header if "SYS / # / OBS TYPES" in l]
+    ## clean SYS / # / OBS TYPES
+    lines_sys = [l[:60] for l in lines_sys]
+
+    ## manage the 2 lines systems
+    for il, l in enumerate(lines_sys):
+        if l[0] == " ":
+            lines_sys[il - 1] = lines_sys[il - 1] + l
+            lines_sys.remove(l)
+
+    #### store system and observables in a dictionnary
+    dict_sys_nobs = dict()
+    dict_sys_cols = dict()  # adds the prn, and LLI and SSI indicators
+    obs_all_list = []
+
+    for il, l in enumerate(lines_sys):
+        sysobs = l.split()
+        # sysobs[0] = system letter
+        # sysobs[1] = observables number
+        # sysobs[2:] = observables names
+
+        dict_sys_nobs[sysobs[0]] = int(sysobs[1])
+        ## adds the LLI and SSI indicators
+        obs_w_lli_ssi = [(e, e + "_LLI", e + "_SSI") for e in sysobs[2:]]
+        obs_all_list.extend([e for sublist in obs_w_lli_ssi for e in sublist])
+
+        dict_sys_cols_tmp = [("prn",)] + obs_w_lli_ssi
+        dict_sys_cols[sysobs[0]] = [e for sublist in dict_sys_cols_tmp for e in sublist]
+
+        if len(sysobs[2:]) != int(sysobs[1]):
+            log.warning(
+                "difference between theorectical and actual obs nbr in the header"
+            )
+
+    ## the max number of observable (for the reading)
+    nobs_max = max(dict_sys_nobs.values())
+
+    df_all_stk = []
+    #### reading the epochs
+    for iepoc in tqdm(range(len(epochs)), desc="Reading " + filename):
+        epoch = epochs[iepoc, 0]
+        ## define the start/end indices of the epoch block
+        iline_start = epochs[iepoc, 1] + 1
+        if iepoc == len(epochs) - 1:
+            iline_end = None
+        else:
+            iline_end = epochs[iepoc + 1, 1]
+
+        lines_epoc = [re.sub(r"[\r\n]+", "", e) for e in lines[iline_start:iline_end]]
+
+        # .replace("\r", "").replace("\n", "") i.e. re.sub(r"[\r\n]+", "", s) IS NOT .strip() !!
+        # .strip() removes the spaces at the beginning and end of the string, and they should be kept!
+
+        ## read the epoch block using pandas' fixed width reader
+        b = StringIO("\n".join(lines_epoc))
+
+        columns_width = [3] + nobs_max * [14, 1, 1]
+        df_epoch = pd.read_fwf(b, header=None, widths=columns_width)
+
+        # one add the system to use groupby
+        #df_epoch_sysadded = df_epoch.copy()
+        #df_epoch_sysadded["systmp"] = df_epoch_sysadded[0].str[0]
+
+        df_epoch_ok_stk = []
+
+        ## slow method
+        #### assign the correct observable names for each system
+        # for sys in dict_sys_cols.keys():
+        # #for sys, df_epoch_sys in df_epoch_sysadded.groupby("systmp"):
+        #     df_epoch_sys = df_epoch[df_epoch[0].str[0] == sys]
+        #     df_epoch_sys_clean = df_epoch_sys.iloc[:, : len(dict_sys_cols[sys])]
+        #     df_epoch_sys_clean.columns = dict_sys_cols[sys]
+        #     df_epoch_ok_stk.append(df_epoch_sys_clean)
+
+        ## faster method (post 2025-01)
+        #df_epoch['sys'] = df_epoch[0].str.strip().str[0]
+
+        #df_epoch_ok = pd.concat(df_epoch_ok_stk)
+        # An epoch column is created to fasten the process
+        col_epoch = pd.Series([epoch] * len(df_epoch), name="epoch")
+        df_epoch_ok = pd.concat([col_epoch, df_epoch], axis=1)
+
+        df_all_stk.append(df_epoch_ok)
+
+    ## final concat and cosmetic (reorder columns, sort)
+    df_rnx_obs = pd.concat(df_all_stk)
+
+    ## extract the system
+    df_rnx_obs["sys"] = df_rnx_obs[0].str.strip().str[0]
+
+    #### assign the correct observable names for each system
+    df_rnx_obs_ok_stk = []
+    for sys, df_rnx_obs_sys in df_rnx_obs.groupby("sys"):
+        df_rnx_obs_sys_clean = df_rnx_obs_sys.iloc[:, : len(dict_sys_cols[sys])]
+        df_rnx_obs_sys_clean.columns = dict_sys_cols[sys]
+        df_rnx_obs_ok_stk.append(df_rnx_obs_sys_clean)
+
+    df_rnx_obs_ok = pd.concat(df_rnx_obs_ok_stk)
+
+    col_sys = pd.Series(df_rnx_obs_ok["prn"].str[0], name="sys")
+    col_prni = pd.Series(df_rnx_obs_ok["prn"].str[1:], name="prni", dtype=int)
+    df_rnx_obs_ok = pd.concat([df_rnx_obs_ok, col_sys, col_prni], axis=1)
+
+    main_cols = ["epoch", "sys", "prn", "prni"]
+    obs_used_list = list(set(df_rnx_obs_ok.columns).difference(set(main_cols)))
+    df_rnx_obs_ok = df_rnx_obs_ok.reindex(main_cols + list(sorted(obs_used_list)), axis=1)
+
+    df_rnx_obs_ok.sort_values(["epoch", "prn"], inplace=True)
+    df_rnx_obs_ok.reset_index(drop=True, inplace=True)
+
+    if set_index:
+        df_rnx_obs_ok.set_index(set_index, inplace=True)
+        df_rnx_obs_ok.sort_index(inplace=True)
+
+    return df_rnx_obs_ok
+
 
 ############ UTILITY FUNCTIONS
+
+
+def open_rinex(rnx_inp,verbose=False):
+    #### open block
+    try:
+        rnx_wrk = hatanaka.decompress(rnx_inp)
+    except:
+        rnx_wrk = rnx_inp
+        pass
+
+    lines = utils.open_readlines_smart(rnx_wrk, verbose=verbose)
+    epochs = operational.rinex_read_epoch(rnx_wrk, out_index=True)
+    if type(rnx_inp) is str or type(rnx_inp) is pathlib.Path:
+        filename = os.path.basename(rnx_inp)
+    else:
+        filename = "unknown filename"
+
+    return lines, epochs, filename
 
 
 def df_rnx_clean_lli_ssi(df_rnx_in):
@@ -469,6 +599,16 @@ def _sats_find(lines_inp):
             return bloc_tuple
 
 
+def _header_reader(lines_inp):
+    #### Split header and Observation body
+    i_end_header = 0
+    for il, l in enumerate(lines_inp):
+        if "END OF HEADER" in l:
+            i_end_header = il
+            break
+    return lines_inp[: i_end_header + 1]
+
+
 def _line_reader(linein, nobs):
     """
     DISCONTINUED
@@ -489,23 +629,33 @@ def _line_reader(linein, nobs):
         else:
             try:
                 out_stk.append(float(out))
-            except:
+            except ValueError:
                 out_stk.append(np.nan)
 
     return out_stk
 
+if __name__ == "__main__" and True:
 
-# if __name__ == "__main__":
-#     p = "/home/psakicki/aaa_FOURBI/RINEXexemple/smne176z.18o"
-#     # /home/psakicki/aaa_FOURBI/RINEXexemple/mlvl176z.18o"
-#
-#     time_readrinex = """
-# from __main__ import read_rinex2_obs
-# p = "/home/psakicki/Desktop/daej2220.04o"
-# read_rinex2_obs(p)
-# """
-#
-#     import timeit
-#
-#     t = timeit.timeit(time_readrinex, number=10)
-#     print(t)
+
+    #p2 = "/home/psakicki/Desktop/daej2220.04o"
+    #df2 = read_rinex2_obs(p2)
+    #print(df2)
+
+    p3 = "/home/psakicki/aaa_FOURBI/RINEXexemple/ENOG00REU_R_20243030000_01D_30S_MO.rnx"
+    df3 = read_rinex3_obs(p3)
+    print(df3)
+
+    time_readrinex = """
+from __main__ import read_rinex2_obs
+p = "/home/psakicki/aaa_FOURBI/RINEXexemple/smne176z.18o"
+p = "/home/psakicki/aaa_FOURBI/RINEXexemple/mlvl176z.18o"
+p = "/home/psakicki/Desktop/daej2220.04o"
+read_rinex2_obs(p)
+"""
+
+    import timeit
+
+    #t = timeit.timeit(time_readrinex, number=10)
+    #print(t)
+
+
