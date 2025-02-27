6abf04d4cef495d782eb9b5ce381ac91ff4762e5
PSTPX1 <psakicki@TPX1-GFZ>
2025-02-27 21:32:19 +0100

    apply_async

--- geodezyx/operational/gins_runner/gins_dirs_run.py
+++ geodezyx/operational/gins_runner/gins_dirs_run.py
@@ -183,7 +183,7 @@ def run_dirs_multi(
 
     pool = mp.Pool(processes=nprocs)
     #res_raw = [pool.apply_async(run_dirs_kwwrap, args=(x,)) for x in kwargs_lis]
-    res_raw = pool.map_async(run_dirs_kwwrap, kwargs_lis)
+    res_raw = pool.map(run_dirs_kwwrap, kwargs_lis)
 
     return None
 
