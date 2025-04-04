08852bbd0545261065fc4b54625fa3563577e4c1
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-04-04 16:52:27 +0200

    Adjust date range in spotgins_run to include one day buffer

--- geodezyx/operational/gins_runner/spotgins_run.py
+++ geodezyx/operational/gins_runner/spotgins_run.py
@@ -119,7 +119,7 @@ def spotgins_run(
     if not no_updatebd or not no_concat_orb_clk:
         rnxs_dates = [conv.rinexname2dt(e) for e in rnxs_path_use]
         rnxs_dates = [e for e in rnxs_dates if not e is None]
-        date_min, date_max = min(rnxs_dates), max(rnxs_dates)
+        date_min, date_max = min(rnxs_dates) - dt.datetime(days=1), max(rnxs_dates) + dt.datetime(days=1)
 
     ##### Update the database ################
     if not no_updatebd:
