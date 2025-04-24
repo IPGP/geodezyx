7a3eec25a0837a64eaa5fba2531b4b50646fe4f5
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-04-24 16:46:40 +0200

    Handle ValueError in RINEX reading and fallback to filename for start/end dates

--- geodezyx/operational/gins_runner/gins_dirs_gen.py
+++ geodezyx/operational/gins_runner/gins_dirs_gen.py
@@ -240,7 +240,12 @@ def gen_dirs_rnxs(
         # date
         try:
             srt_epo, end_epo, freq_rnx = operational.rinex_start_end(rnx_path, True, verbose=verbose)
-        except:
+        except ValueError as e:
+            log.warning("Unable to read RINEX, fallback to filename to guess its start/end")
+            srt_epo = conv.rinexname2dt(rnx_path)
+            end_epo = srt_epo + dt.timedelta(days=1)
+            freq_rnx = 30
+        finally:
             srt_epo, end_epo, freq_rnx = None, None, None
             bool_cntu = _fail_rnx(rnx_path, rnx_dt, "get RINEX start/end failed")
             if bool_cntu:
