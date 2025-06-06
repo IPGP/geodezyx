378ba10b530d3e8743a8de456aa611af95238465
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-06-06 11:59:21 +0200

    netcdf fixed

--- geodezyx/marine/netcdf_a0a/read_netcdf_exemple/read_netcdf_exemple.py
+++ geodezyx/marine/netcdf_a0a/read_netcdf_exemple/read_netcdf_exemple.py
@@ -10,6 +10,8 @@ import netCDF4
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+####### IFREMER 
 p = "./netcdf_exemples/104577.nc"
 ncfile_ifremer = netCDF4.Dataset(p,mode='r',format='NETCDF4_CLASSIC')
 print(ncfile_ifremer)
@@ -22,6 +24,7 @@ time = time.squeeze()
 plt.plot(time,pres)
 ncfile_ifremer["LATITUDE"][:]
 
+####### NOAA
 p = "./netcdf_exemples/wc32_19910624to19920604.nc"
 ncfile_noaa = netCDF4.Dataset(p,mode='r',format='NETCDF4_CLASSIC')
 print(ncfile_noaa)
@@ -29,7 +32,7 @@ print(ncfile_noaa)
 ncfile_noaa["timeSeries"]
 ncfile_noaa["pressure_sensor_info"]
 
-
+####### HOMEMADE
 p_homemade = "/home/psakicki/CODES/geodezyx/geodezyx/marine/netcdf_a0a/output_netcdf/out.nc"
 ncfile_homemade = netCDF4.Dataset(p_homemade,mode='r',format='NETCDF4_CLASSIC')
 print(ncfile_homemade)
