1d89234b311523391f96f4564be3fa80b72c9848
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-08-01 17:19:21 +0200

    fix: add detailed docstring for PRIDE PPPAR process function in pride_pppar_frontend.py

--- geodezyx/spytgins/files_classes/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from .master_station_file import *
-from .offsets_file import *
-from .station_file import *
--- geodezyx/spytgins/spotgins_utils/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from .spotgins_utils import *
-from .functions import *
