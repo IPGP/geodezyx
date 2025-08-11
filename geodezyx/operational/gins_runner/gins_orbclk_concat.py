dec60618b8b703184ffdba12d6377f4a101625ae
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-08-11 18:03:27 +0200

    feat: add initial implementation of gins_orbclk_concat and remove gins_prairie modules

---
+++ geodezyx/operational/gins_runner/gins_orbclk_concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 10/08/2025 11:38:34
+
+@author: psakic
+"""
---
+++ geodezyx/operational/gins_runner/legacy/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 10/08/2025 11:44:01
+
+@author: psakic
+"""
