7fb0ef8906144491bf1252f0db4c36d40a3a9b46
PiSa on TPX1 <psakicki@TPX1-GFZ>
2026-01-05 19:40:13 +0100

    feat: rename parameters in run_testgins function for clarity

--- geodezyx/operational/gins_runner/testgins_run.py
+++ geodezyx/operational/gins_runner/testgins_run.py
@@ -9,36 +9,35 @@ Created on 05/01/2026 19:12:11
 from geodezyx.operational.gins_runner import get_rnx_eost, singugins_run
 from geodezyx import utils
 
-
-def run_testgins(dir_res_testgins=None, dir_rnxs_testgins=None):
+def run_testgins(results_folder=None, rnxs_folder=None):
     """
     Executes the test GINS workflow by downloading RINEX files and running the GINS process.
 
     Parameters:
-    dir_res_testgins (str, optional): Path to the directory where the results will be stored.
+    results_folder (str, optional): Path to the directory where the results will be stored.
         If not provided, a timestamped folder under '/root/030_RESULTS/' will be created.
-    dir_rnxs_testgins (str, optional): Path to the directory where input RINEX files will be
+    rnxs_folder (str, optional): Path to the directory where input RINEX files will be
         downloaded or read. Defaults to '/root/020_BDRNX/rnxs_testGINS_from_EOST' if not provided.
 
     Returns:
     None
     """
     # If no directory for RINEX files is provided, use the default path
-    if not dir_rnxs_testgins:
-        dir_rnxs_testgins = "/root/020_BDRNX/rnxs_testGINS_from_EOST"
+    if not rnxs_folder:
+        rnxs_folder = "/root/020_BDRNX/rnxs_testGINS_from_EOST"
 
     # Download or read RINEX files for the specified years
-    get_rnx_eost(dir_rnxs_testgins, year_start=2022, year_end=2023)
+    get_rnx_eost(rnxs_folder, year_start=2022, year_end=2023)
 
     # If no results directory is provided, create a timestamped folder
-    if not dir_res_testgins:
+    if not results_folder:
         timstp = utils.get_timestamp()
-        dir_res_testgins = "/root/030_RESULTS/testGINS_" + timstp
+        results_folder = "/root/030_RESULTS/testGINS_" + timstp
 
     # Run the GINS process with the specified directories
     singugins_run(
-        results_folder=dir_res_testgins,
-        bdrnx_folder=dir_rnxs_testgins,
+        results_folder=results_folder,
+        bdrnx_folder=rnxs_folder,
     )
 
     # The function does not return any value
@@ -52,7 +51,7 @@ def main(argv=None):
 
     parser.add_argument(
         "-o",
-        "--dir_res_testgins",
+        "--results_folder",
         type=str,
         default=None,
         help=(
@@ -62,7 +61,7 @@ def main(argv=None):
     )
     parser.add_argument(
         "-r",
-        "--dir_rnxs_testgins",
+        "--rnxs_folder",
         type=str,
         default=None,
         help=(
@@ -73,10 +72,9 @@ def main(argv=None):
 
     args = parser.parse_args(argv)
     run_testgins(
-        dir_res_testgins=args.dir_res_testgins,
-        dir_rnxs_testgins=args.dir_rnxs_testgins,
+        results_folder=args.results_folder,
+        rnxs_folder=args.rnxs_folder,
     )
-
 
 if __name__ == "__main__":
     main()
