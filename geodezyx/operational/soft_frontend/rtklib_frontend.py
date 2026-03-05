a931880f7b8509850a806f5a09485b09f4440d4d
Pierre Sakic <sakic@ipgp.fr>
2026-03-05 16:21:32 +0100

    parquet is mandatory not more test

--- geodezyx/operational/soft_frontend/rtklib_frontend.py
+++ geodezyx/operational/soft_frontend/rtklib_frontend.py
@@ -270,9 +270,8 @@ def rtklib_run_mono(
     else:
         utils.gzip_compress(out_res_fil + ".stat", rm_inp=True)
         out_prq_fil = out_res_fil.replace(".out", ".parquet")
-        if not os.path.isfile(out_prq_fil):
-            df_out2prq = files_rw.read_rtklib(out_res_fil, return_df=True)
-            df_out2prq.to_parquet(out_prq_fil, engine='auto')
+        df_out2prq = files_rw.read_rtklib(out_res_fil, return_df=True)
+        df_out2prq.to_parquet(out_prq_fil, engine='auto')
         log.info("RTKLIB RUN OK for {} :)".format(exp_full_name))
 
     if not keep_tmp:
