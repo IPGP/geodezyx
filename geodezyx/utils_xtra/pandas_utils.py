parenthesis in pandas_utils.py to ensure correct time unit conversion

geodezyx/utils_xtra/pandas_utils.py
@@ -176,7 +176,7 @@ def diff_pandas(df, col_name, use_np_diff=False):
     if not use_np_diff:
         out = df[col_name].diff() / df[col_name].index.to_series().diff().dt.total_seconds()
     else:
-        dif = np.diff(df[col_name].values) / np.diff(df.index).astype(np.float32) * 10 ** -9 ## because it is in nanosec per def
+        dif = np.diff(df[col_name].values) / (np.diff(df.index).astype(np.float32) * 10 ** -9) ## because it is in nanosec per def
         out = pd.Series(np.insert(dif,0,np.nan),  ## add NaN as the 1st value
                         index=df[col_name].index,
                         name=col_name)