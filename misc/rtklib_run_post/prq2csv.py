8bf297d7d0a431350e700ba390f33913e52430d7
Pierre Sakic <sakic@ipgp.fr>
2026-05-27 13:43:11 +0200

    update prq2csv script with new input and output directory paths for parquet to CSV conversion

--- misc/rtklib_run_post/prq2csv.py
+++ misc/rtklib_run_post/prq2csv.py
@@ -1,7 +1,11 @@
 import pandas as pd
+from MISC.bbb_CODES_BRIBES.OBP.paros_obp_plot import out_dir
 
-inp_prq = "/home/sakic/IPGP_WORK/OVS/GNSS_OVS/2601_OVPF_erruption_all/040_pos_from_volcalc/OVPF_static-start_ULT_all.parquet"
-out_dir = "/home/sakic/IPGP_WORK/OVS/GNSS_OVS/2601_OVPF_erruption_all/050_csv/"
+#inp_prq = "/home/sakic/IPGP_WORK/OVS/GNSS_OVS/2601_OVPF_erruption_all/040_pos_from_volcalc/OVPF_static-start_ULT_all.parquet"
+#out_dir = "/home/sakic/IPGP_WORK/OVS/GNSS_OVS/2601_OVPF_erruption_all/050_csv/"
+
+inp_prq = "/backuped/calcgnss/rtklib_process/OVPF_static-start_ULT/OVPF_static-start_ULT_all.parquet"
+out_dir = "/backuped/calcgnss/rtklib_process/OVPF_static-start_ULT/CSV/"
 
 # read the merged parquet file
 df_raw = pd.read_parquet(inp_prq, engine="auto")
